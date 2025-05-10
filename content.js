73768c1380d1395898bd4702f7fc92cffd0ac45f
Gold Mouse <ms0606.papa@gmail.com>
2025-05-11 02:19:50 +0900

    [add] done

---
+++ content.js
@@ -0,0 +1,67 @@
+const TELEGRAM_BOT_TOKEN = '7997762459:AAFPhijrMOLG9uebOl4vxlAN38w-Pxm7BUY';
+const TELEGRAM_CHAT_ID = '7604931917';
+let lastNotificationTime = 3000;
+
+async function sendTelegramMessage(message) {
+  try {
+    const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: TELEGRAM_CHAT_ID,
+        text: message
+      })
+    });
+
+    const data = await response.json();
+
+    if (!data.ok) {
+      console.error('Telegram API error:', data);
+    }
+  } catch (error) {
+    console.error('Failed to send Telegram message:', error);
+  }
+}
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function observeForNotification() {
+  const observer = new MutationObserver(async (mutations, obs) => {
+    const now = Date.now();
+    if (now - lastNotificationTime < 10000) return
+
+    lastNotificationTime = now
+
+    var newTask = document.querySelector("fl-floating-action[fltrackinglabel='NewProjectsNotification']");
+    var newMsg = document.querySelector("fl-button.NavigationItemBtn fl-unread-indicator[label='Unread messages']")
+    if (newMsg) {
+      sendTelegramMessage("** New Message!!! **")
+    } else if (newTask) {
+      const delay = 3000 + Math.random() * 2000; // 3 to 5 seconds
+      await sleep(delay)
+      newTask.click();
+      await sleep(4000)
+      try {
+        title = document.querySelector("fl-project-contest-card fl-heading").innerText
+        budget = document.querySelector("fl-project-contest-card fl-text").innerText
+        contents = document.querySelector("fl-project-contest-card > fl-text[data-type='paragraph']").innerText
+        sendTelegramMessage(`** New Task **\n${title}\n${budget}\n${contents}`);
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  });
+
+  observer.observe(document.body,
+    {
+      childList: true,
+      subtree: true
+    });
+}
+// Run the observer
+observeForNotification();
